27655e94a7a6c366ea4cdd37f259728f283d331d
Taiki Sakamoto <godgourd@gmail.com>
2023-02-18 17:17:45 +0900

    Update calljnifunc.c

--- libtest/calljnifunc.c
+++ libtest/calljnifunc.c
@@ -1,10 +1,16 @@
 #include "calljnifunc.h"
 
 const char* CallJNIGetStringUTFChars(JNIEnv *env, jstring str, jboolean *isCopy) {
+  if (str == NULL) {
+    return NULL;
+  }
   return (*env)->GetStringUTFChars(env, str, isCopy);
 }
 
 void CallJNIReleaseStringUTFChars(JNIEnv *env, jstring str, const char *utf) {
+  if (str == NULL) {
+    return;
+  }
   (*env)->ReleaseStringUTFChars(env, str, utf);
 }
 
